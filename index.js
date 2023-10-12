de8d2f01765496fde857417b7d1a72d98956a263
richie20502 <richie.lugo.recillas.1990@gmail.com>
2023-10-12 00:55:18 -0600

    se agregaron rutas de render vistas con pug

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import express from "express";
 import userRoutes from './routes/userRoutes.js';
 const app = express();
 const port = 3000;
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-app.use('/', userRoutes);
+app.use('/auth', userRoutes);
 
 
 app.listen(port, () => {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
 const router = express.Router();
 
-router.get('/', function(req, res){
-    res.send('bienvenido desde rutas user');
+router.get('/login', function(req, res){
+    res.render('auth/login');
 });
 
 router.route('/')
